c5f7fd9e0e8059492b4bcf0d9e5b4337205de07d
Stanley Gu <stanleygu@gmail.com>
2014-11-16 19:03:39 -0800

    Returning error messages from sbml or sbml if valid

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
+# import tellurium as te
+import libsbml
 
 app = Flask(__name__, static_url_path='')
-
 
 @app.route('/')
 def root():
@@ -12,7 +13,15 @@ def root():
 def translate():
     data = request.get_json()
     sbml = data.get('sbml')
-    return sbml
+    doc = libsbml.readSBMLFromString(sbml.encode('utf-8'))
+    if doc.getNumErrors() > 0:
+        messages = ['Invalid SBML!\n']
+        for i in xrange(0, doc.getNumErrors()):
+            error = doc.getError(i)
+            messages.append(error.getMessage())
+        return ''.join(messages)
+    else:
+        return sbml
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
